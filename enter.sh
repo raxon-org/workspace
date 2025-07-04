f3758e03eef0ce78ed3917c8d4bcf304e4f24200
remco van der Velde <remco.pc@outlook.com>
2025-07-04 15:24:48 +0200

    Update Docker exec command to use raxon-workspace-0-0-1 in enter.sh, install.sh, and reinstall.sh

--- enter.sh
+++ enter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec -it docker-12.7-core /bin/bash
+docker exec -it raxon-workspace-0-0-1 /bin/bash
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker-compose up -d --build
-docker exec -it docker-12.7-core /bin/bash
+docker exec -it raxon-workspace-0-0-1 /bin/bash
