c0285da8bb816ff9bc37af33d125303f625aa757
remco van der Velde <remco.pc@outlook.com>
2025-07-07 17:05:37 +0200

    init route imports

---
+++ setup.workspace.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+app raxon/node object import -class=System.Route -url=/Application/Mount/Domain/Admin.Workspace.Com/Data/System.Route
+app raxon/node object import -class=System.Route -url=/Application/Mount/Domain/Api.Workspace.Com/Data/System.Route
