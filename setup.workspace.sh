598fab449ad5826b6d4a5e8642df8ae6eb733c14
remco van der Velde <remco.pc@outlook.com>
2025-07-29 15:25:37 +0200

    init

--- setup.workspace.sh
+++ setup.workspace.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-app raxon/node object import -class=System.Route -url=/Application/Mount/Domain/Admin.Workspace.Com/Data/System.Route
-app raxon/node object import -class=System.Route -url=/Application/Mount/Domain/Api.Workspace.Com/Data/System.Route
+app raxon/node object import -class=System.Route -url=/Application/Mount/Domain/Admin.Workspace.Com/Data/System.Route.json
+app raxon/node object import -class=System.Route -url=/Application/Mount/Domain/Api.Workspace.Com/Data/System.Route.json
+app install raxon/account -patch
+app install raxon/doctrine -patch
+app raxon/doctrine 
