e335673ae27a5fb5c4ef272eec803332999c0f23
Remco van der Velde <remco@universeorange.com>
2026-01-10 15:19:01 +0100

    pp

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -354,7 +354,7 @@ trait Main {
         $command = Core::binary($object) . ' raxon/basic apache2 site has -server.name=\'' . $options->server->name . '\'';
         $output = [];
         exec($command, $output);
-        $output = implode('', $output);
+        $output = implode(PHP_EOL, $output);
         d($output);
         if($output !== 'true') {
             $command = Core::binary($object) . ' raxon/basic apache2 site create -server.admin=\'' . $options->server->admin . '\' -server.name=\'' . $options->server->name . '\' -development';
