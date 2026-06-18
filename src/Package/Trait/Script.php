b27b2082cd219e3602ccb59b2a4bb12eceaac9d7
Remco van der Velde <remco@universeorange.com>
2026-06-18 17:58:41 +0200

    init

--- src/Package/Trait/Script.php
+++ src/Package/Trait/Script.php
@@ -5,8 +5,14 @@ trait Script {
     public function add($flags, $options): void
     {
         $object = $this->object();
-        $url = $object->config('controller.dir.data');
-        d($url);
+        d($flags);
+        d($options);
+        d($object->config('project.dir.domain'));
+        d($object->config('project.dir'));
+
+
+//        $url = $object->config('controller.dir.data');
+//        d($url);
 
     }
 }
